bd3971f993429d58178bcec1227d976595e57d99
Ryan Thomson <git@ryanthomson.net>
2021-12-06 20:45:41 -0500

    Add solution for day 6

---
+++ day6/part1.swift
@@ -0,0 +1,28 @@
+import Foundation
+
+let inputURL = URL(fileURLWithPath: "input.txt", relativeTo: URL(fileURLWithPath: #filePath))
+let input = try! String(contentsOf: inputURL).trimmingCharacters(in: .whitespacesAndNewlines)
+
+var state = [Int: Int]()
+for number in input.split(separator: ",").compactMap({ Int($0) }) {
+    state[number, default: 0] += 1
+}
+
+for _ in 0..<80 {
+    state = tick(state: state)
+}
+
+print(state.values.reduce(0, +))
+
+func tick(state: [Int: Int]) -> [Int: Int] {
+    var newState = [Int: Int]()
+    for (age, count) in state {
+        if age == 0 {
+            newState[8, default: 0] += count // Reset these fish
+            newState[6, default: 0] += count // New fish
+        } else {
+            newState[age - 1, default: 0] += count
+        }
+    }
+    return newState
+}
